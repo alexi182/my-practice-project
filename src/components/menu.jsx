00fd792ce06baffe2d38ccd79edb8501102cfc42
alex182 <format255@bk.ru>
2017-05-02 08:35:12 +0300

    '1'

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -21,6 +21,7 @@ export default class Menu extends React.Component {
              <li>
                 <Link to="/bla" class="menu-link">Bla</Link>
              </li>
+     {/*        <p>{this.props.newName}</p>*/}
           </ul>
       );
    }
--- src/pages/help.jsx
+++ src/pages/help.jsx
@@ -1,46 +1,67 @@
-import './help.css';
-import { autobind } from 'core-decorators';
+import Menu from '../components/menu';
+import "./help.css";
+import {autobind} from "core-decorators";
+
+
 @autobind()
-
 export default class Help extends React.Component {
-    constructor(props) {
-        super(props);
+   constructor(props) {
+      super(props);
 
-        this.name = "Will";
-        this.count = "300";
+      this.name = "Will";
+      this.count = "300";
 
-        this.state = {
-            text: this.props.text,
-            count: this.count
-        };
-    }
+      this.state = {
+         text: this.props.text,
+         count: this.count
+      };
+   }
 
-    handleChange(e) {
-        this.setState({
-            text: e.target.value,
-            count: this.count - e.target.value.length
-        });
-    }
+   handleChange(e) {
+      this.setState({
+         text: e.target.value,
+         count: this.count - e.target.value.length
+      });
+   }
 
-    render() {
-        console.log(this.props);
-        return (
-            <div className="help-content">
-                <h2>Раздел помощи</h2>
+   render() {
+      return (
+          <div className="help-content">
+             <h2>Раздел помощи</h2>
 
-                <textarea placeholder="Текст сообщения" className="form-control" value={this.state.text} onChange={this.handleChange} />
+             <textarea placeholder="Текст сообщения" className="form-control" value={this.state.text} onChange={this.handleChange} />
 
-                <div className="count-remains">
-                    Максимальное число символов:{this.state.count}
+             <div className="count-remains">
+                Максимальное число символов:{this.state.count}
+             </div>
+
+             <div>
+                <p>Текст: {this.state.text}</p>
+                <p>Имя: {this.name}</p>
+             </div>
+             {/*  <Menu newName={this.name} />*/}
+
+             <div className="price-block">
+                <div className="price-block__item">
+                   <span></span>
                 </div>
-
-                <div>
-                    <p>Текст: {this.state.text}</p>
-                    <p>Имя: {this.name}</p>
-                    <input type="text" />
+                <div className="price-block__item">
+                   <span></span>
                 </div>
-            </div>
-        );
-    }
+                <div className="price-block__item">
+                   <span></span>
+                </div>
+                <div className="price-block__item">
+                   <span></span>
+                </div>
+                <div className="price-block__item">
+                   <span></span>
+                </div>
+             </div>
+
+          </div>
+      );
+      console.log(this.props);
+   }
 
 }
